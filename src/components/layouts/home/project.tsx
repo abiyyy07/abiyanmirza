32cc1e119c9a37402ce0f99ec6ff8539f7e7610f
abiyyy07 <abiyy0206@gmail.com>
2025-04-20 15:36:55 +0700

    update: Project

--- src/components/layouts/home/project.tsx
+++ src/components/layouts/home/project.tsx
@@ -20,7 +20,28 @@ export default function ProjectHome() {
                             <h3 className="text-xl font-extrabold text-purple-800 mb-1">Bahasa Bridge</h3>
                             <p className="text-gray-600">Mobile apps for increase and learning English skills</p>
                             <Link
-                            href="" target="_blanc"
+                            href="https://drive.google.com/drive/folders/1FO1fm-M3j6wJAOPTr6SrNSufJmLyIe2T?usp=drive_link" target="_blanc"
+                            className="mt-2 inline-block text-purple-600 font-semibold bg-tranparant border-2 border-purple-800 px-2 py-1 rounded-md hover:bg-purple-800 hover:text-white transform duration-200"
+                            >
+                            View Project
+                            </Link>
+                        </div>
+                    </div>
+                    {/* Project two */}
+                    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+                        <div className="relative pb-2/3">
+                            <Image
+                            src="/projek/afarush.jpg"
+                            alt="Bahasa Bridge"
+                            width={500}
+                            height={500}
+                            />
+                        </div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-extrabold text-purple-800 mb-1">Afa Rush</h3>
+                            <p className="text-gray-600">Mobile apps for typing competition and solo typing</p>
+                            <Link
+                            href="https://drive.google.com/drive/folders/1OiHnBpRHBTK7T2bnPCAJLdVZtBd5B1k-?usp=drive_link" target="_blanc"
                             className="mt-2 inline-block text-purple-600 font-semibold bg-tranparant border-2 border-purple-800 px-2 py-1 rounded-md hover:bg-purple-800 hover:text-white transform duration-200"
                             >
                             View Project
@@ -28,8 +49,7 @@ export default function ProjectHome() {
                         </div>
                     </div>
                 </div>
-                <div className="mt-4 flex">
-                    <Link href={'/project'} className="bg-transparent hover:bg-purple-600 px-2 py-1 border-2 border-white text-white hover:border-purple-600 transform duration-200 font-semibold">Other project</Link>
+                <div className="mt-4 flex"><Link href={'/project'} className="bg-transparent hover:bg-purple-600 px-2 py-1 border-2 border-white text-white hover:border-purple-600 transform duration-200 font-semibold">Other project</Link>
             </div>
         </>
     )
